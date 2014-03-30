68566cde14f4b5cf83f2ffdd703e17f86b76cbc1
Sjors Provoost <sjors@sprovoost.nl>
2014-03-30 12:09:57 +0200

    Export to JSON

--- app/models/radical.rb
+++ app/models/radical.rb
@@ -70,118 +70,62 @@ class Radical < ActiveRecord::Base
     end
   end
   
-  def self.export_radicals(f)
-    self.where(is_synonym: false).where("frequency < ?", 100).order(frequency: :desc).limit(100).each do |first_radical|
-      f << "@autoreleasepool {\n"
-      puts "#{ first_radical }..."
-      f << "  NSLog(@\"#{ first_radical }...\");\n"
-      f << "  r = [NSEntityDescription insertNewObjectForEntityForName:kEntityRadical\n"  
-      f << "               inManagedObjectContext:managedObjectContext];\n"
-      f << "  r.isFirstRadical = @YES;\n"
-      f << "  r.simplified = @\"#{ first_radical.simplified }\";\n"
-      f << "  r.rank = @#{ first_radical.rank };\n"
-      f << "  r.section = @#{ 0 };\n"  
-      f << "\n"
-      primary_second_radicals = Radical.where("id in (?)", first_radical.radicals).to_a #.slice(0,5) # DEBUG
-      self.export_second_radicals(primary_second_radicals, f, false, first_radical)
-
-      self.export_save_context(f)
-      f << "}\n" # End of auto release pool
-    end
+  ##########
+  # Export #
+  ##########
+  
+  def self.export_radicals_characters(f)
+    res = self.where(is_synonym: false).where("frequency < ?", 100).order(frequency: :desc).limit(100).collect{|first_radical|
+      {
+        simplified: first_radical.simplified,
+        rank: -first_radical.frequency,
+        second_radicals: Radical.where("id in (?)", first_radical.radicals).collect{|second_radical|
+          {
+            simplified: second_radical.simplified,
+            rank: -second_radical.frequency, # TODO: frequency given first radical
+            characters: first_radical.with_synonym_characters.keep_if{|c| c.has_radicals(first_radical, second_radical)}.collect {|character|
+              {
+                simplified: character.simplified,
+                rank: character.rank
+              }
+            } 
+          }         
+        }
+      }
+    }
     
+    f << res.to_json
   end
   
-  def self.export_save_context(f)
-    f << "\n"
-    f << "  r = nil;\n"
-    f << "  r2 = nil;\n"
-    f << "  error = nil;\n"
-    f << "  [managedObjectContext save:&error];\n"
-    f << "  if(error != nil) { NSLog(@\"%@\", error); abort();}\n"
-    f << "  [managedObjectContext reset];\n"
-    f << "\n\n"
-  end
-  
-  def self.export_second_radicals(second_radicals, f, without_first_radical, first_radical)
-    second_radicals.each do |second_radical|
-      f << "  r2 = [NSEntityDescription insertNewObjectForEntityForName:kEntityRadical\n"  
-      f << "               inManagedObjectContext:managedObjectContext];\n"
-      f << "  r2.isFirstRadical = @NO;\n"
-      
-      if without_first_radical
-        f << "  r2.section = @2;\n"  
-      else
-        f << "  r2.firstRadical = r;\n"
-        f << "  r2.section = @#{ 0 };\n"  
-      end
-      f << "  r2.simplified = @\"#{ second_radical.simplified }\";\n"
-      f << "  r2.rank = @#{ second_radical.rank };\n"
-      @characters = first_radical.with_synonym_characters.keep_if{|c| c.has_radicals(first_radical, second_radical)}
-    
-      self.export_characters(f, @characters, second_radical)
-    end
-  end
-  
-  def self.export_characters(f, characters, second_radical)
-    f << "  for(NSArray *character_words in @[\n" 
-    character_count = characters.count
-    characters.each_index do |k| 
-      character = characters[k]
-      f << "    @["
-      f << "@[@\"#{ character.simplified }\", @#{ character.rank }], "
-            
-      f << "@[" + character.words.collect{|w| "@[@\"#{ w.simplified }\", @\"#{ w.english.collect{| e | e.gsub("\"","\\\"")}.join('; ') }\"]" }.join(", ") + "]"
-      if k <= character_count - 2
-        f << "],\n"
-      else
-        f << "]\n"
-      end
-    end
-    f << "  ]) {\n"
-    f << "    Character* c;\n"
-    f << "    NSArray *character = [character_words firstObject];\n"
-    f << "    c = [Character fetchBySimplified:[character firstObject] inManagedObjectContext:managedObjectContext includesPropertyValuesAndSubentities:NO];\n"
-    f << "    if( c==nil ) {\n"
-    f << "      c = [NSEntityDescription insertNewObjectForEntityForName:kEntityCharacter inManagedObjectContext:managedObjectContext];\n"
-    f << "      c.simplified = [character firstObject];\n"
-    f << "      c.rank = [character lastObject];\n"
-    f << "      for(NSArray *simplified_english in [character_words lastObject]) {\n"
-    f << "        Word* w;\n"
-    f << "        w = [Word fetchBySimplified:[simplified_english firstObject] inManagedObjectContext:managedObjectContext includesPropertyValuesAndSubentities:NO];\n"
-    f << "        if( w==nil ) {\n"
-    f << "          w = [NSEntityDescription insertNewObjectForEntityForName:kEntityWord inManagedObjectContext:managedObjectContext];\n"
-    f << "          w.simplified = [simplified_english firstObject];\n"
-    f << "          w.english = [simplified_english lastObject];\n"
-    f << "          w.wordLength = [NSNumber numberWithInt:[w.simplified length]];\n" 
-    f << "        }\n"
-    f << "        [w addCharactersObject:c];\n"
-    f << "      }\n"
-    f << "    }\n"
-    if second_radical.present?
-      f << "    [c addSecondRadicalsObject:r2];\n"
-    end
-    f << "  }\n"
-    f << "\n"
+  def self.export_characters_words(f)
+    res = Character.all.keep_if{|c| c.radicals.count > 0}.collect{|c|
+      {
+        simplified: c.simplified,
+        words: c.words.collect{|w|
+          {
+            simplified: w.simplified,
+            english: w.english.join('; ') 
+            # english: w.english.collect{| e | e.gsub("\"","\\\"")}.join('; ') 
+          }
+        }
+      }
+    }  
+    f << res.to_json  
   end
   
   def self.export_synonyms(f)
-    f << "+(void)synonyms:(NSManagedObjectContext *)managedObjectContext {\n"
-    f << "  NSLog(@\"Synonyms\");\n"
-    f << "  for(Radical *radical in [Radical all:managedObjectContext]) {\n"
-    Radical.all.each do |r|
-      if r.synonyms.count > 0 && r.simplified != "阝" && r.simplified != "罒"
-        f << "    if ([radical.simplified isEqualToString:@\"#{ r.simplified }\"]) {\n"
-        f << "      radical.synonyms = @\"#{ Radical.where("id in (?)", r.synonyms).collect{| s | s.simplified}.join(" ") }\";\n"
-        f << "    }\n"
-      end
-    end
-    f << "  }\n"
-    f << "                                  \n"
-    f << "  NSError *error;\n"
-    f << "  [managedObjectContext save:&error];\n"
-    f << "  if(error) { NSLog(@\"%@\",error); abort();}\n"
-    f << "}\n"
+    res = Radical.all.keep_if{|r|  r.synonyms.count > 0 && r.simplified != "阝" && r.simplified != "罒"}.collect{|r|
+      {
+        simplified: r.simplified,
+        synonyms: Radical.where("id in (?)", r.synonyms).collect{|s| s.simplified}.join(" ")
+        # synonyms: Radical.where("id in (?)", r.synonyms).collect{|s|
+        #   {
+        #     simplified: s.simplified
+        #   }
+        # }
+      }
+    }
+    f << res.to_json  
     
   end
-  
 end
--- lib/tasks/export.rake
+++ lib/tasks/export.rake
@@ -1,40 +1,23 @@
 namespace :export do 
-  desc "Export radicals, characters and words for use in Core Data"
-  task :obj_c => :environment do 
-    File.open("/Users/sjors/Dropbox/Kangxi/iOs/KangxiRadicals/Kangxi Radicals/import.h", 'w') do |f| 
-      f << "#import <UIKit/UIKit.h>\n"
-      f << "#import \"Radical.h\"\n"
-      f << "#import \"Character.h\"\n"
-      f << "#import \"Word.h\"\n"
-      f << "\n"
-      f << "@interface Populator : NSObject\n"
-      f << "+(void)import:(NSManagedObjectContext *)managedObjectContext;\n"
-      f << "+(void)synonyms:(NSManagedObjectContext *)managedObjectContext;\n"
-      f << "@end\n"
+  task :all => [:radicals_characters,:characters_words,:synonyms]
+  
+  desc "Export radicals, characters, words and synomyms"
+  
+  task :radicals_characters => :environment do 
+    File.open("/Users/sjors/Dropbox/Kangxi/iOs/KangxiRadicals/Kangxi Radicals/radicals_characters.json", 'w') do |f| 
+      Radical.export_radicals_characters(f)
     end
-    
-    
-    File.open("/Users/sjors/Dropbox/Kangxi/iOs/KangxiRadicals/Kangxi Radicals/import.m", 'w') do |f| 
-      f << "#import \"import.h\"\n"
-      f << "@implementation Populator\n"
+  end
+  
+  task :characters_words => :environment do 
+    File.open("/Users/sjors/Dropbox/Kangxi/iOs/KangxiRadicals/Kangxi Radicals/characters_words.json", 'w') do |f| 
+      Radical.export_characters_words(f)
+    end
+  end
 
-      f << "+(void)import:(NSManagedObjectContext *)managedObjectContext {\n"
-      f << "  NSError *error;\n"
-      f << "  Radical* r;\n"
-      f << "  Radical* r2;\n"
-    
-      Radical.export_radicals(f)
-      
-      f << "}\n"
-      
-      puts "Synonyms..."
-      f << "//*********** \n"
-      f << "// Synonyms * \n" 
-      f << "//*********** \n"
-      
+  task :synonyms => :environment do 
+    File.open("/Users/sjors/Dropbox/Kangxi/iOs/KangxiRadicals/Kangxi Radicals/synonyms.json", 'w') do |f| 
       Radical.export_synonyms(f)
-      
-      f << "@end\n"
     end
   end
 end
